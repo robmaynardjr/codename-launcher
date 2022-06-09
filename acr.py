c4f4e5f3dc21b4a63fe913f3c4c898303c62f186
RM <robmaynardjr@gmail.com>
2022-06-09 13:44:14 -0500

    init 3

--- acr.py
+++ acr.py
@@ -1,23 +1,21 @@
 from hashlib import new
 from msilib.schema import Class
+from typing_extensions import Self
 from azure.containerregistry import ContainerRegistryClient
 from azure.cli.core import get_default_cli
 
-class Create_ACR:
-     
+class Create_ACR():     
     # Create Registry
-        def create_acr(acr_name, region, replication, sku, admin):
-            region = ('az account list-locations -o table')
-            acr_name = input("")
-            region = ()
-            replication = bool
-            replication_region = repl_region
-            sku = ("")
-            admin = bool        
-            if exit_code == 0:
-                print ("ACR created")
-            else: 
-                print(logs)
-
+    def create_acr(Self.acr_url, region, replication, sku, admin):
+        Self.acr_url = input("tst")
+        region = ('')
+        replication = bool
+        eplication_region = repl_region
+        sku = ("")
+        admin = bool        
+        if exit_code == 0:
+            print ("ACR created")
+        else: 
+            print(logs)
 
 createAcr = ACR.create_acr('maynard1', 'centralus', False, 'Standard', True)
