a785b54ee2a8068cb84606820c1f2130d75de914
RM <robmaynardjr@gmail.com>
2022-06-09 17:28:47 -0500

    acr.py

--- acr.py
+++ acr.py
@@ -4,18 +4,19 @@ from typing_extensions import Self
 from azure.containerregistry import ContainerRegistryClient
 from azure.cli.core import get_default_cli
 
-class Create_ACR():     
-    # Create Registry
-    def create_acr(Self.acr_url, region, replication, sku, admin):
-        Self.acr_url = input("tst")
-        region = ('')
-        replication = bool
-        eplication_region = repl_region
-        sku = ("")
-        admin = bool        
+class Create_ACR(acr_url, acr_region, replication, sku, admin):
+        # Create Registry
+    def create_acr(acr_url, region, replication, sku, admin):
+        
+        self.acr_url = input("tst")
+        self.acr_region = ('')
+        self.replication = bool
+        self.eplication_region = repl_region
+        self.sku = ("")
+        self.admin = bool
         if exit_code == 0:
             print ("ACR created")
         else: 
             print(logs)
 
-createAcr = ACR.create_acr('maynard1', 'centralus', False, 'Standard', True)
+createAcr = create_acr('maynard1', 'centralus', False, 'Standard', True)
