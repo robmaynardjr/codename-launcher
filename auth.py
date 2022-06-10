6ade6169d5d86284da9a1cf28879f211732fcd94
RM <robmaynardjr@gmail.com>
2022-06-10 05:57:45 -0500

    auth

--- auth.py
+++ auth.py
@@ -1,18 +1,14 @@
 from azure.identity import DefaultAzureCredential
-from azure.identity import AzureCliCredential, ChainedTokenCredential, ManagedIdentityCredential
-class auth:
-    
-    def config_creds():
-        default_credential = DefaultAzureCredential()
-        DefaultAzureCredential(exclude_interactive_browser_credential=False)
-        DefaultAzureCredential(managed_identity_client_id=client_id)
-        managed_identity = ManagedIdentityCredential()
-        azure_cli = AzureCliCredential()
-        credential_chain = ChainedTokenCredential(managed_identity, azure_cli)
-        client = EventHubProducerClient(namespace, eventhub_name, credential_chain)
-        
-        credential = DefaultAzureCredential()
-        credential = DefaultAzureCredential()
-        async wiht credentials
-        
-        
+from azure.mgmt.resource.resources import ResourceManagementClient
+
+credential = DefaultAzureCredential()
+
+client = ResourceManagementClient(
+    credential=credential,
+    subscription_id=input("YOUR_SUBSCRIPTION_ID"
+)
+
+for resource_group in client.resource_groups.list():
+    print(f"Resource group: {resource_group.name}")
+
+print(f"Successful credential: {credential._successful_credential.__class__.__name__}")
