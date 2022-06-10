83acd155b149cee6be64f87866d5bd85bd659797
RM <robmaynardjr@gmail.com>
2022-06-10 03:16:07 -0500

    init

--- acr.py
+++ acr.py
@@ -6,17 +6,20 @@ from azure.cli.core import get_default_cli
 from az.cli import az
 
 class Create_ACR:
-        acr_url = input("tst")
-        replication = bool
-        replication_region = repl_region
-        sku = ("")
-        admin = bool
-        acr_region = input('')
+        
         # Create Registry
-                
-        def create_acr(acr_url, acr_region, replication_region, sku, admin): 
-                     
-            if exit_code == 0:  
-                print ("ACR created")
-            else: 
-                print(logs)
+        def create_acr(acr_url, acr_region, replication, replication_region, sku, admin):
+            acr_url = input("tst")
+            replication = input(bool)
+            replication_region = input(repl_region)
+            sku = input("")
+            admin = input(bool)
+            acr_region = input('')
+            args=[acr_url, replication, replication_region, sku, admin, acr_region]
+            cli = get_default_cli()
+            if cli.invoke(args):
+                return cli.result.result
+            elif cli.result.error:
+                raise cli.result.error
+            return True    
+            
