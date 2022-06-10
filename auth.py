359bfb49931095afa36f11bda22e87116f27deb1
RM <robmaynardjr@gmail.com>
2022-06-10 05:20:13 -0500

    add auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,18 @@
 from azure.identity import DefaultAzureCredential
-
-default_credential = DefaultAzureCredential()
-
+from azure.identity import AzureCliCredential, ChainedTokenCredential, ManagedIdentityCredential
+class auth:
+    
+    def config_creds():
+        default_credential = DefaultAzureCredential()
+        DefaultAzureCredential(exclude_interactive_browser_credential=False)
+        DefaultAzureCredential(managed_identity_client_id=client_id)
+        managed_identity = ManagedIdentityCredential()
+        azure_cli = AzureCliCredential()
+        credential_chain = ChainedTokenCredential(managed_identity, azure_cli)
+        client = EventHubProducerClient(namespace, eventhub_name, credential_chain)
+        
+        credential = DefaultAzureCredential()
+        credential = DefaultAzureCredential()
+        async wiht credentials
+        
+        
